3822b07675b5c8390c4c0eb15ce3d75b29bdc1f4
amotoso <47503554+amotosouoc@users.noreply.github.com>
2020-02-08 04:20:17 +0100

    Update Broodlord.lua

--- NPCModules/BlackwingLair/Broodlord.lua
+++ NPCModules/BlackwingLair/Broodlord.lua
@@ -3,16 +3,16 @@ if not LibStub then return end
 local ThreatLib, MINOR = LibStub("LibThreatClassic2", true)
 if not ThreatLib then return end
 
-local BROODLORD_ID = 12017
+local BROODLORD_LASHLAYER_ID = 12017
 local KNOCK_AWAY_ID = 18670
 
-ThreatLib:GetModule("NPCCore-r"..MINOR):RegisterModule(BROODLORD_ID, function(Broodlord)
-	function Broodlord:Init()
-        self:RegisterCombatant(BROODLORD_ID, true)
-		self:RegisterSpellDamageHandler(BROODLORD_ID, KNOCK_AWAY_ID, self.KnockAway)
+ThreatLib:GetModule("NPCCore-r"..MINOR):RegisterModule(BROODLORD_LASHLAYER_ID, function(BroodlordLashlayer)
+	function BroodlordLashlayer:Init()
+        self:RegisterCombatant(BROODLORD_LASHLAYER_ID, true)
+		self:RegisterSpellDamageHandler(BROODLORD_LASHLAYER_ID, KNOCK_AWAY_ID, self.KnockAway)
 	end
 
-	function Broodlord:KnockAway(sourceGUID, unitId)
+	function BroodlordLashlayer:KnockAway(sourceGUID, unitId)
         self:ModifyThreat(sourceGUID, unitId, 0.5, 0)
 	end
 end)
