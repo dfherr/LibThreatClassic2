6b3a725eb7c9ff6c78c1bf85d705b2ec46a02061
amotoso <47503554+amotosouoc@users.noreply.github.com>
2020-02-08 04:28:08 +0100

    Update BroodlordLashlayer.lua

--- NPCModules/BlackwingLair/BroodlordLashlayer.lua
+++ NPCModules/BlackwingLair/BroodlordLashlayer.lua
@@ -1,5 +1,4 @@
 if not _G.THREATLIB_LOAD_MODULES then return end -- only load if LibThreatClassic2.lua allows it
-if not LibStub then return end
 local ThreatLib, MINOR = LibStub("LibThreatClassic2", true)
 if not ThreatLib then return end
 
