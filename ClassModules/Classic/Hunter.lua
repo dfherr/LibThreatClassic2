c08997c0351b06fe4a63bba14bb8dfcca8dce3a1
Dennis-Florian Herr <deflo.herr@gmail.com>
2020-04-12 01:25:48 +0200

    fix hunter feign death resist handling

--- ClassModules/Classic/Hunter.lua
+++ ClassModules/Classic/Hunter.lua
@@ -77,9 +77,12 @@ function Hunter:DisengageMiss(spellID, target)
 	self:AddTargetThreat(target, -(amt * self:threatMods()))
 end
 
--- Feign is a rather unique case. It's cast on all targets, but may be resisted by any one target. There is no combat log message - only an error event with ERR_FEIGN_DEATH_RESISTED from GlobalStrings
--- ERR_FEIGN_DEATH_RESISTED always happens before SPELLCAST_SUCCESSFUL, so we "prime" FD when we get SENT, then invalidate it if we get a resist, let it through otherwise.
--- The net effect is that a resist on any one target invalidates the threat reset on all targets, but we can't help that since we don't have target data on who resisted
+-- Feign is a rather unique case. It's cast on all targets, but may be resisted by any one target. 
+-- There is no combat log message - only an error event with ERR_FEIGN_DEATH_RESISTED from GlobalStrings
+-- ERR_FEIGN_DEATH_RESISTED always happens before UNIT_SPELLCAST_SENT, so we "prime" FD when we get SENT, 
+-- then invalidate it if we get a resist, let it through otherwise.
+-- The net effect is that a resist on any one target invalidates the threat reset on all targets, 
+-- but we can't help that since we don't have target data on who resisted
 local FeignDeathPrimed = 0
 function Hunter:FeignDeath()
 	if GetTime() - FeignDeathPrimed < 5 then
@@ -97,7 +100,8 @@ function Hunter:UNIT_SPELLCAST_SENT(event, unit, target, castGUID, spellID)
 	end
 end
 
-function Hunter:UI_ERROR_MESSAGE(event, msg)
+function Hunter:UI_ERROR_MESSAGE(event, errorType, msg)
+	-- ThreatLib:Debug("ui error %s %s %s", event, errorType, msg)
 	if msg == ERR_FEIGN_DEATH_RESISTED then
 		ThreatLib:Debug("Canceling FD!")
 		FeignDeathPrimed = 0
